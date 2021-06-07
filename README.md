ad87e63bfa8fec88c773b351119e21151cf34776
Andrzej Heczko <andrzej.heczko@gmail.com>
2021-06-07 10:29:07 +0200

    fix: install.sh - wait for db, added cron:instal and db port to setup:install

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Magento 2 docker workspace
 
 Nginx and php 7.4 for Magento 2.4
+
+## Prerequisites
+
+docker and docker-compose must be ready on your system.
+
+## Quick start
+
+Just run setup.sh and wait till it ends.
+
+## Customizations
+
+Feel free to copy docker-compose.yml.stub to docker-compose.yml and customize by your needs.
+
+Runner dockerfile is prepared for magento
--- docker/runner/scripts/install.sh
+++ docker/runner/scripts/install.sh
@@ -4,9 +4,11 @@ cd /var/www || exit 1
 
 composer install
 
+wait-for-db $DB_HOST $DB_PORT $DB_USER "$DB_PASSWORD"
+
 bin/magento setup:install \
   --base-url=http://localhost/ \
-  --db-host=$DB_HOST \
+  --db-host=$DB_HOST:$DB_PORT \
   --db-name=$DB_NAME \
   --db-user=$DB_USER \
   --db-password="$DB_PASSWORD" \
@@ -22,6 +24,8 @@ bin/magento admin:user:create \
   --admin-email="$ADMIN_EMAIL" \
   --admin-user="$ADMIN_USER" \
   --admin-password="$ADMIN_PASSWORD" \
+
+sudo -u www-data bin/magento cron:install
 
 refresh.sh
 
